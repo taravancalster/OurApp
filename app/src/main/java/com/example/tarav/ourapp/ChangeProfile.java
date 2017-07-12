05ec1a9e5e50fa49e78491589c2e31a156c664e3
Ellyn737 <e.reese@mail.de>
2017-07-12 11:25:57 +0200

    changeProfile

---
+++ app/src/main/java/com/example/tarav/ourapp/ChangeProfile.java
@@ -0,0 +1,13 @@
+package com.example.tarav.ourapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile);
+    }
+}
--- app/src/main/java/com/example/tarav/ourapp/SignUp.java
+++ app/src/main/java/com/example/tarav/ourapp/SignUp.java
@@ -1,7 +1,9 @@
 package com.example.tarav.ourapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void toProfile(View view){
+        startActivity(new Intent(this, Profile.class));
+    }
+
+    public void signIn(View view){
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
