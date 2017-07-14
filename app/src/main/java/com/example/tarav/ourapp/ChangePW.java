e113ee1d918306d6a8cf0ef06d0a747e94130edf
Ellyn737 <e.reese@mail.de>
2017-07-14 15:03:05 +0200

    changePW

--- app/src/main/java/com/example/tarav/ourapp/ChangePW.java
+++ app/src/main/java/com/example/tarav/ourapp/ChangePW.java
@@ -1,7 +1,10 @@
 package com.example.tarav.ourapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChangePW extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class ChangePW extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_pw);
+
+        //define the buttons
+        Button buttonSave = (Button)findViewById(R.id.button17);
+        Button buttonCancel = (Button)findViewById(R.id.button18);
+
+        //set an OnClickListener to the buttons
+        buttonSave.setOnClickListener(onClickListener);
+        buttonCancel.setOnClickListener(onClickListener);
+
     }
+
+    //what happens onClick?
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.button17){
+                //save the new password
+            }
+
+            else if(v.getId() == R.id.button18){
+                //go back to Change Profile
+                startActivity(new Intent(ChangePW.this, ChangeProfile.class));
+            }
+        }
+    };
 }
