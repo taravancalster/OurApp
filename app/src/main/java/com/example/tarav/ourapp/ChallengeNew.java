64e2c98ef09a3700af19e46e47cd2dc6e4d75542
taravancalster <tara.vancalster@hotmail.com>
2018-03-28 15:24:46 +0200

    updated ChallengeNew
    defined all the elements in ChallengeNew.java
    both buttons are working now

--- app/src/main/java/com/example/tarav/ourapp/ChallengeNew.java
+++ app/src/main/java/com/example/tarav/ourapp/ChallengeNew.java
@@ -1,13 +1,54 @@
 package com.example.tarav.ourapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ChallengeNew extends AppCompatActivity {
+
+    TextView challengeCategory ,challengeTitle, challengeDescription;
+    ImageView challengePicture;
+    Button tryButton, notNowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_new);
+
+        //define TextViews
+        challengeCategory = (TextView) findViewById(R.id.textView17);
+        challengeTitle = (TextView) findViewById(R.id.textView18);
+        challengeDescription = (TextView) findViewById(R.id.textView19);
+        //define ImageView
+        challengePicture = (ImageView) findViewById(R.id.imageView4);
+        //define Buttons
+        tryButton = (Button) findViewById(R.id.button20);
+        notNowButton = (Button) findViewById(R.id.button19);
+
+        //add OnClick events
+        tryButton.setOnClickListener(onClickListener);
+        notNowButton.setOnClickListener(onClickListener);
     }
+
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            //if tryButton is clicked: change to Challenge
+            if(v.getId() == R.id.button20){
+                startActivity(new Intent(ChallengeNew.this, Challenge.class));
+                //set new challenge in profile
+            }
+
+            //if notNotButton is clicked: change to Profile
+            if (v.getId() == R.id.button19){
+                startActivity(new Intent(ChallengeNew.this, Profile.class));
+            }
+        }
+    };
 }
